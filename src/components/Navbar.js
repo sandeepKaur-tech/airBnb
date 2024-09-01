88b01f04e7c1ea9807c420ae7db9153559850688
sandeepKaur-tech <kaur94sandeep@gmail.com>
2024-09-01 14:54:52 +0400

    create files

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import airbnb from "../images/airbnb-logo.png"
+
+export default function Navbar(){
+    return (
+        <>    
+        <img src={airbnb}/>
+        </>
+    )
+}
